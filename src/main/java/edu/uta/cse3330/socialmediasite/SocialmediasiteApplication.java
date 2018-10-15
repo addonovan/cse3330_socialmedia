334ca20fc202cdbbad16b4f9bdd3dccb6a2abaff
addonovan <austin@addonovan.com>
2018-10-15 17:50:27 -0500

    initial commit
    Generated with spring-initializr:
    https://start.spring.io/

---
+++ src/main/java/edu/uta/cse3330/socialmediasite/SocialmediasiteApplication.java
@@ -0,0 +1,12 @@
+package edu.uta.cse3330.socialmediasite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediasiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediasiteApplication.class, args);
+	}
+}
