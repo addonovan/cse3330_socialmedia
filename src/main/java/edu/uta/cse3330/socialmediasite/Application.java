8aed2cd6a919ab2aab8419415927c200cf8ee1e4
addonovan <austin@addonovan.com>
2018-10-15 18:04:18 -0500

    rename SocialmediasiteApplication => Application

--- src/main/java/edu/uta/cse3330/socialmediasite/Application.java
+++ src/main/java/edu/uta/cse3330/socialmediasite/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SocialmediasiteApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SocialmediasiteApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
