f643cba7d90d17074acd9ae385ff653d93984b55
addonovan <austin@addonovan.com>
2018-10-16 12:26:00 -0500

    add basic connection skeleton

--- src/main/java/edu/uta/cse3330/socialmediasite/DatabaseDriver.java
+++ src/main/java/edu/uta/cse3330/socialmediasite/DatabaseDriver.java
@@ -2,11 +2,33 @@ package edu.uta.cse3330.socialmediasite;
 
 import edu.uta.cse3330.socialmediasite.model.Profile;
 
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public final class DatabaseDriver {
+
+    private static final String CONNECTION_STRING = "jdbc:postgresql://localhost/SocialMedia";
+
+    private static final Connection CONNECTION;
+
+    static {
+        Properties props = new Properties();
+        props.setProperty("user", "oh no");
+        props.setProperty("password", "oh no");
+        props.setProperty("ssl", "true");
+
+        try {
+            CONNECTION = DriverManager.getConnection(CONNECTION_STRING, props);
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to open database connection!", e);
+        }
+    }
+
 
     public static List<Profile> listProfiles() {
         List<Profile> profiles = new ArrayList<>();
