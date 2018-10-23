8e10f095e31d7b164b68dcaee076d6b0a12dc35c
addonovan <austin@addonovan.com>
2018-10-22 22:55:39 -0500

    update up and down files for new tables

--- src/main/resources/db/down.sql
+++ src/main/resources/db/down.sql
@@ -1,12 +1,16 @@
 -- Drop in reverse order of creation
 REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA "public" FROM "application";
 
-DROP TABLE GroupMessage;
-DROP TABLE GroupMember;
+DROP TABLE DirectMessage;
 DROP TABLE "Group";
-DROP TABLE PostLikes;
+DROP TABLE PollAnswers;
+DROP TABLE PollPost;
+DROP TABLE PostReaction;
+DROP TABLE RefEmotion;
 DROP TABLE Post;
-DROP TABLE PageCategory;
+DROP TABLE EventInterest;
+DROP TABLE Event;
+DROP TABLE PageInvite;
 DROP TABLE PageAdmin;
 DROP TABLE PageLike;
 DROP TABLE Page;
--- src/main/resources/db/up.sql
+++ src/main/resources/db/up.sql
@@ -1,7 +1,9 @@
+--
 -- Create all Tables
+--
 
 CREATE TABLE Profile (
-  ProfileId           SERIAL          PRIMARY KEY,
+  Id                  SERIAL          PRIMARY KEY,
   FirstName           VARCHAR(100)    NOT NULL,
   LastName            VARCHAR(100)    NOT NULL,
   PhoneNumber         CHAR(12)        NOT NULL,
@@ -9,74 +11,130 @@ CREATE TABLE Profile (
   UserName            VARCHAR(100)    NOT NULL,
   Password            VARCHAR(100)    NOT NULL,
   CreatedTime         TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  ProfileActive       BOOL            NOT NULL DEFAULT TRUE
+  Active              BOOL            NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE Page (
-  PageId              SERIAL          PRIMARY KEY,
-  PageName            VARCHAR(100)    NOT NULL,
-  PageDescription     VARCHAR(512)    NOT NULL,
-  HeaderImageURL      VARCHAR(2083)   NOT NULL DEFAULT '//pages/headers/default.png',
-  ProfileImageURL     VARCHAR(2083)   NOT NULL DEFAULT '//pages/pictures/default.png',
-  PageActive          BOOL            NOT NULL DEFAULT TRUE
+  Id                  SERIAL          PRIMARY KEY,
+  Name                VARCHAR(100)    NOT NULL,
+  Description         VARCHAR(512)    NOT NULL,
+  HeaderImageURL      VARCHAR(2083)   NOT NULL DEFAULT '//media/pages/default_header.png',
+  ProfileImageURL     VARCHAR(2083)   NOT NULL DEFAULT '//media/pages/default_profile.png',
+  Active              BOOL            NOT NULL DEFAULT TRUE
 );
 
+-- Profile/Page Relationships
 CREATE TABLE PageLike (
-  ProfileId           INTEGER         NOT NULL REFERENCES Profile(ProfileId),
-  PageId              INTEGER         NOT NULL REFERENCES Page(PageId)
+  ProfileId           INTEGER         NOT NULL REFERENCES Profile(Id),
+  PageId              INTEGER         NOT NULL REFERENCES Page(Id),
 );
 
 CREATE TABLE PageAdmin (
-  ProfileId           INTEGER         NOT NULL REFERENCES Profile(ProfileId),
-  PageId              INTEGER         NOT NULL REFERENCES Page(PageId)
+  ProfileId           INTEGER         NOT NULL REFERENCES Profile(Id),
+  PageId              INTEGER         NOT NULL REFERENCES Page(Id)
 );
 
-CREATE TABLE PageCategory (
-  PageId              INTEGER         NOT NULL REFERENCES Page(PageId),
-  CATEGORY            VARCHAR(20)     NOT NULL
+CREATE TABLE PageInvite (
+  ProfileId           INTEGER         NOT NULL REFERENCES Profile(Id),
+  SenderProfileId     INTEGER         NOT NULL REFERENCES Profile(Id),
+  PageId              INTEGER         NOT NULL REFERENCES Page(Id)
+)
+
+-- Event
+CREATE TABLE Event (
+  Id                  SERIAL          PRIMARY KEY,
+  PageId              INTEGER         NOT NULL REFERENCES Page(Id),
+  Name                VARCHAR(100)    NOT NULL,
+  Description         VARCHAR(4096)   NOT NULL DEFAULT '',
+  StartTime           TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  EndTime             TIMESTAMP       NOT NULL,
+  Location            VARCHAR(512)    NOT NULL,
 );
 
+CREATE TABLE EventInterest (
+  EventId             INTEGER         NOT NULL REFERENCES Event(Id),
+  ProfileId           INTEGER         NOT NULL REFERENCES Profile(Id),
+  Attending           BOOL            NOT NULL
+);
+
+-- Posts
 CREATE TABLE Post (
-  PostId              SERIAL          PRIMARY KEY,
-  PageId              INTEGER         NOT NULL REFERENCES Page(PageId),
-  ProfileId           INTEGER         REFERENCES Profile(ProfileId),
-  Message             VARCHAR(1024),
-  AttachmentURL       VARCHAR(2083),
-  ParentPostId        INTEGER         REFERENCES Post(PostId),
-  CreatedTime         TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP
+  Id                  SERIAL          PRIMARY KEY,
+  PosterId            INTEGER         REFERENCES Profile(Id),
+  PageId              INTEGER         NOT NULL REFERENCES Page(Id),
+  ParentPostId        INTEGER         REFERENCES Post(Id),
+  Message             VARCHAR(4096),
+  MediaURL            VARCHAR(2083),
 );
 
-CREATE TABLE PostLikes (
-  ProfileId           INTEGER         NOT NULL REFERENCES Profile(ProfileId),
-  PageId              INTEGER         NOT NULL REFERENCES Page(PageId)
+CREATE TABLE RefEmotion (
+  Id                  SERIAL          PRIMARY KEY,
+  Name                VARCHAR(32)     NOT NULL,
+  Image               VARCHAR(2083)   NOT NULL
 );
 
+CREATE TABLE PostReaction (
+  PostId              INTEGER         NOT NULL REFERENCES Post(Id),
+  ProfileId           INTEGER         NOT NULL REFERENCES Profile(Id),
+  EmotionId           INTEGER         NOT NULL REFERENCES RefEmotion(Id),
+);
+
+-- Poll Posts
+CREATE TABLE PollPost (
+  PostId              INTEGER         PRIMARY KEY REFERENCES Post(Id),
+  Question            VARCHAR(128)    NOT NULL,
+  EndTime             TIMESTAMP       NOT NULL,
+);
+
+CREATE TABLE PollAnswers (
+  Id                  SERIAL          PRIMARY KEY,
+  PostId              INTEGER         REFERENCES PollPost(PostId),
+  AnswerText          VARCHAR(128)    NOT NULL
+);
+
+CREATE TABLE PollVotes (
+  PollAnswerId        INTEGER         NOT NULL REFERENCES PollAnswers(Id),
+  ProfileId           INTEGER         NOT NULL REFERENCES Profile(Id)
+);
+
+-- Group & GMs
 CREATE TABLE "Group" (
-  GroupId             SERIAL          PRIMARY KEY,
-  GroupName           VARCHAR(128)    NOT NULL,
-  GroupPictureURL     VARCHAR(2083)   NOT NULL DEFAULT '//groups/default.png',
-  GroupDescription    VARCHAR(512)    NOT NULL DEFAULT ''
+  Id                  SERIAL          PRIMARY KEY,
+  Name                VARCHAR(64)     NOT NULL,
+  Description         VARCHAR(256)    NOT NULL DEFAULT '',
+  PictureURL          VARCHAR(2083)   NOT NULL DEFAULT '//media/groups/default.png'
 );
 
-CREATE TABLE GroupMember (
-  GroupId             INTEGER         NOT NULL REFERENCES "Group"(GroupId),
-  ProfileId           INTEGER         NOT NULL REFERENCES Profile(ProfileId)
+CREATE TABLE DirectMessage (
+  GroupId             INTEGER         NOT NULL REFERENCES "Group"(Id),
+  SenderProfileId     INTEGER         NOT NULL REFERENCES Profile(Id),
+  Message             VARCHAR(4096),
+  MediaURL            VARCHAR(2083)
 );
 
-CREATE TABLE GroupMessage (
-  SenderProfileId     INTEGER         NOT NULL REFERENCES Profile(ProfileId),
-  GroupId             INTEGER         NOT NULL REFERENCES "Group"(GroupId),
-  Message             VARCHAR(1024),
-  AttachmentURL       VARCHAR(2083),
-  CreatedTime         TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
+--
 -- Grant permissions
+--
+
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA "public" TO "application";
 
+--
 -- Insert Testing Data
+--
+
+INSERT INTO RefEmotion
+  (Name, Image)
+  VALUES
+  ('Like', '//media/reactions/like.png'),
+  ('Anger', '//media/reactions/anger.png'),
+  ('Dislike', '//media/reactions/dislike.png'),
+  ('Love', '//media/reactions/love.png');
 
 INSERT INTO Profile
   (FirstName, LastName, PhoneNumber, Email, UserName, Password)
   VALUES
-  ('Austin', 'Donovan', '81772999079', 'austin@addonovan.com', 'addonovan', 'password1');
+  ('Austin', 'Donovan', '8179911052', '', 'addonovan', 'password1'),
+  ('Giannina', 'Pachas', '8179911052', '', 'giannapachas', 'password2'),
+  ('Emily', 'Knowles', '8179911052', '', 'elknowles', 'password3'),
+  ('Joseph', 'Brewster', '8179911052', '', 'jojettison', 'password4');
+
