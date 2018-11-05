a39b05f24ad260627f85edb5645b73d5e6bca279
addonovan <austin@addonovan.com>
2018-11-04 19:05:03 -0600

    reformat profile_overview to include stylesheets

--- src/main/java/com/addonovan/cse3330/controller/ProfileController.kt
+++ src/main/java/com/addonovan/cse3330/controller/ProfileController.kt
@@ -6,12 +6,10 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
 
 @Controller
+@RequestMapping("/profile")
 open class ProfileController {
 
-    @GetMapping(value = ["/accounts"], produces = ["application/json"])
-    fun accounts() = DatabaseDriver.listAccounts()
-
-    @GetMapping(value = ["/profile/{id}"])
+    @GetMapping(value = ["/{id}"])
     fun profileOverview(@PathVariable("id") id: Int, model: Model): String {
         val profile = DatabaseDriver.getProfileById(id)
 
