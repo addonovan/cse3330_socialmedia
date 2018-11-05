864a911bfacc5d8b5404e539022099df1320ca06
addonovan <austin@addonovan.com>
2018-11-05 15:18:29 -0600

    add new sp_up.sql file
    This will contain all of the stored procedures used in the database.

---
+++ src/main/resources/db/sp_up.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE FUNCTION CreateProfile(
+    Email       "Account".Email%TYPE,
+    PhoneNumber "Account".PhoneNumber%TYPE,
+    FirstName   "Profile".FirstName%TYPE,
+    LastName    "Profile".LastName%TYPE,
+    Username    "Profile".Username%TYPE,
+    Password    "Profile".Password%TYPE
+) RETURNS INTEGER
+LANGUAGE plpgsql
+AS $$
+
+    DECLARE
+        account_id INTEGER := -1;
+
+    BEGIN
+
+        INSERT INTO "Account"
+          (email, phonenumber, isprivate)
+        VALUES
+          (@Email, @PhoneNumber, FALSE)
+        RETURNING Id INTO account_id;
+
+        INSERT INTO "Profile"
+          (accountid, firstname, lastname, username, password, languageid)
+        VALUES
+          (@account_id, @FirstName, @LastName, @Username, @Password, 1);
+
+        RETURN account_id;
+
+    END
+$$;
