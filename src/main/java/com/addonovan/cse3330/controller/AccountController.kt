e28370364262cbab98a04b8df8b17d08b43f9b48
addonovan <austin@addonovan.com>
2018-11-06 16:55:22 -0600

    set up AccountController for page creation

--- src/main/java/com/addonovan/cse3330/controller/AccountController.kt
+++ src/main/java/com/addonovan/cse3330/controller/AccountController.kt
@@ -1,6 +1,7 @@
 package com.addonovan.cse3330.controller
 
 import com.addonovan.cse3330.DbEngine
+import com.addonovan.cse3330.model.Page
 import com.addonovan.cse3330.model.Profile
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -27,6 +28,21 @@ open class AccountController {
     ) {
         val newId = DbEngine.createProfile(profile).id
         response.sendRedirect("/profile/$newId")
+    }
+
+    @GetMapping("/createPage")
+    fun pageCreationForm(model: Model): String {
+        model.addAttribute("page", Page())
+        return "create_page"
+    }
+
+    @PostMapping("/createPage")
+    fun pageCreationSubmit(
+            response: HttpServletResponse,
+            @ModelAttribute page: Page
+    ) {
+        val newId = DbEngine.createPage(page).id
+        response.sendRedirect("/page/$newId")
     }
 
 }
