a51eea7c258ec454f1ff4092bc563ed68eaa0c5c
addonovan <austin@addonovan.com>
2018-11-07 14:57:26 -0600

    document SqlEntity

--- src/main/java/com/addonovan/cse3330/model/SqlEntity.kt
+++ src/main/java/com/addonovan/cse3330/model/SqlEntity.kt
@@ -2,8 +2,15 @@ package com.addonovan.cse3330.model
 
 import java.sql.ResultSet
 
+/**
+ * An entity in a SQL database.
+ */
 interface SqlEntity {
 
+    /**
+     * Parses this entity from the given [row] in a manner which will not affect
+     * the row itself.
+     */
     fun fromRow(row: ResultSet)
 
 }
