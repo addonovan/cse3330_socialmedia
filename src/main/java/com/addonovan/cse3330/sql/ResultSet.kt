75aed87afba12d620e222096a7e6b5f87f68b160
addonovan <austin@addonovan.com>
2018-11-07 14:54:56 -0600

    document ResultSet

--- src/main/java/com/addonovan/cse3330/sql/ResultSet.kt
+++ src/main/java/com/addonovan/cse3330/sql/ResultSet.kt
@@ -2,11 +2,15 @@ package com.addonovan.cse3330.sql
 
 import java.sql.ResultSet
 
-inline fun <T> ResultSet.map(block: (ResultSet) -> T): List<T> {
+/**
+ * A convenience function to map each row of this [ResultSet] to a specific
+ * type, [T], by using the provided `action`.
+ */
+inline fun <T> ResultSet.map(action: (ResultSet) -> T): List<T> {
     val list = ArrayList<T>()
 
     while (next()) {
-        list += block(this)
+        list += action(this)
     }
 
     return list
