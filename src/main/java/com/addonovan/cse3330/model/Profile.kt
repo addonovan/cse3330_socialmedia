96f7ec1f42c74ac4e32a52bb32090b91226fa15d
addonovan <austin@addonovan.com>
2018-11-07 15:03:40 -0600

    document the Account types

--- src/main/java/com/addonovan/cse3330/model/Profile.kt
+++ src/main/java/com/addonovan/cse3330/model/Profile.kt
@@ -2,16 +2,25 @@ package com.addonovan.cse3330.model
 
 import java.sql.ResultSet
 
+/**
+ * The class model representing the SQL `Profile` entity. This is a
+ * specialization of the [Account] entity.
+ */
 class Profile : Account() {
 
+    /** The first name of this profile's user. */
     lateinit var firstName: String
 
+    /** The last name of this profile's user. */
     lateinit var lastName: String
 
+    /** The unique username of this profile. */
     lateinit var username: String
 
+    /** This user's password (which will probably be hashed in the future) */
     lateinit var password: String
 
+    /** The primary language of this user. */
     var languageId: Int = 0
 
     override fun fromRow(row: ResultSet) {
