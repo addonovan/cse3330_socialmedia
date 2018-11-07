86a7f36dec85347ba072005129c7def9213b75ac
addonovan <austin@addonovan.com>
2018-11-07 13:18:18 -0600

    create PostController for creating new posts

---
+++ src/main/java/com/addonovan/cse3330/controller/PostController.kt
@@ -0,0 +1,29 @@
+package com.addonovan.cse3330.controller
+
+import com.addonovan.cse3330.DbEngine
+import com.addonovan.cse3330.model.Account
+import com.addonovan.cse3330.model.Post
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import javax.servlet.http.HttpServletResponse
+
+@Controller
+@RequestMapping("/post")
+open class PostController {
+
+    @PostMapping("/submit")
+    fun submitPost(
+            response: HttpServletResponse,
+            @RequestParam("posterId") posterId: Int,
+            @ModelAttribute post: Post
+    ) {
+        val account = Account().apply {
+            id = posterId
+        }
+        DbEngine.createPost(account, post)
+    }
+
+}
