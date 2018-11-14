ebb2aebe318e95b9939143a6cec2dcab3a65e265
Austin D. Donovan <austin@addonovan.com>
2018-11-13 20:25:07 -0600

    create basic login/logout featuers (#15)
    This is hilariously insecure (as will soon be obvious), but the active profile
    id is directly stored in a cookie on the client with the name 
    "TotallyInsecureLoggedInProfileId", meaning that anyone who's smart 
    enough to open the dev tools is able to change the profile they're currently
    signed into lol.

---
+++ src/main/java/com/addonovan/cse3330/auth.kt
@@ -0,0 +1,58 @@
+package com.addonovan.cse3330
+
+import com.addonovan.cse3330.model.Profile
+import javax.servlet.http.Cookie
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+/** The name of the login cookie. */
+private const val LOGIN_PROFILE_COOKIE_NAME = "TotallyInsecureLoggedInProfileId"
+
+/**
+ * The profile currently signed in, as per the cookies on the given
+ * [HttpServletRequest]. Hopefully the end-user doesn't know how to edit their
+ * cookies lol.
+ */
+val HttpServletRequest.profile: Profile?
+    get() {
+        val cookie = this.cookies.find {
+            it.name == LOGIN_PROFILE_COOKIE_NAME
+        } ?: return null
+
+        val profileId = cookie.value.toIntOrNull() ?: return null
+        return DbEngine.getProfileById(profileId)
+    }
+
+/**
+ * Attaches a cookie that marks the given [profile] as signed in. yes, this is
+ * hilariously insecure because the person can just change the number in the
+ * cookies on their end, but I don't really care this is a school project.
+ */
+fun HttpServletResponse.signIn(profile: Profile) {
+    val cookie = createCookie(
+            name = LOGIN_PROFILE_COOKIE_NAME,
+            value = profile.id.toString(),
+            maxAgeMins = 60
+    )
+    this.addCookie(cookie)
+}
+
+/**
+ * Replaces the active sign-in cookie with one that contains no data and will
+ * expire immediately.
+ */
+fun HttpServletResponse.signOut() {
+    val cookie = createCookie(
+            name = LOGIN_PROFILE_COOKIE_NAME,
+            value = null,
+            maxAgeMins = 0
+    )
+    this.addCookie(cookie)
+}
+
+private fun createCookie(name: String, value: String?, maxAgeMins: Int) =
+        Cookie(name, value).apply {
+            path = "/cse3330"
+            isHttpOnly = true
+            maxAge = maxAgeMins * 60
+        }
---
+++ src/main/java/com/addonovan/cse3330/controller/SignInController.kt
@@ -0,0 +1,48 @@
+package com.addonovan.cse3330.controller
+
+import com.addonovan.cse3330.DbEngine
+import com.addonovan.cse3330.signIn
+import com.addonovan.cse3330.signOut
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import javax.servlet.http.HttpServletResponse
+
+@Controller
+open class SignInController {
+
+    @GetMapping("/signin")
+    fun signInForm() = "signin/login"
+
+    @PostMapping("/signin")
+    fun signIn(
+            model: Model,
+            response: HttpServletResponse,
+            @RequestParam username: String,
+            @RequestParam password: String
+    ): String {
+        val user = DbEngine.getProfileByUsername(username)
+
+        // if the signin failed, just send 'em back to the login screen
+        if (user == null || user.password != password) {
+            model.addAttribute("errorReason", "Invalid username or password!")
+            return "signin/login"
+        }
+
+        // otherwise, log the user in, then send them to the homepage
+        response.signIn(user)
+        response.sendRedirect("/")
+        return "signin/success"
+    }
+
+    @PostMapping("/signout")
+    fun signOut(response: HttpServletResponse) {
+        // delete the cookie, then send them to the home page
+        response.signOut()
+        response.sendRedirect("/")
+    }
+
+
+}
