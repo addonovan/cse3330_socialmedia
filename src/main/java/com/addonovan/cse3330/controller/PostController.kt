8935b94d788c51df3aba9118f1ba4d27911b0ac1
Austin D. Donovan <austin@addonovan.com>
2018-11-15 17:52:45 -0600

    add support for media attachments to posts (#18)
    This adds the ability to select an image file for upload and attachment on
    a post.
    
    This is done by adding the `@RequestParam mediaFile: MultipartFile`
    parameter to the `/post/submit` endpoint, as well as a file selector with
    the `mediaFile` name in the corresponding form.
    
    This has also lifted both the post-display and post-writing sections of
    `account/overview` and `home/feed` into their own templates, `post`
    and `post_box`, respectively.

--- src/main/java/com/addonovan/cse3330/controller/PostController.kt
+++ src/main/java/com/addonovan/cse3330/controller/PostController.kt
@@ -1,12 +1,16 @@
 package com.addonovan.cse3330.controller
 
 import com.addonovan.cse3330.DbEngine
+import com.addonovan.cse3330.UploadType
 import com.addonovan.cse3330.model.Post
 import com.addonovan.cse3330.profile
+import com.addonovan.cse3330.writeAs
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.multipart.MultipartFile
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -25,9 +29,16 @@ open class PostController {
     fun submitPost(
             request: HttpServletRequest,
             response: HttpServletResponse,
+            @RequestParam mediaFile: MultipartFile,
             @ModelAttribute post: Post
     ) {
         response.sendRedirect(request.getHeader("referer"))
+
+        // if we have a file, then we'll save it and add it to the post here
+        if (!mediaFile.isEmpty) {
+            val url = mediaFile.writeAs(UploadType.PostAttachment)
+            post.media = Post.MediaBody(url)
+        }
 
         val user = request.profile ?: return
         post.posterId = user.id
---
+++ src/main/java/com/addonovan/cse3330/uploads.kt
@@ -0,0 +1,92 @@
+package com.addonovan.cse3330
+
+import org.springframework.web.multipart.MultipartFile
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
+
+/** The relative path to the local directory where all media is saved */
+private const val LOCAL_DIR: String = "src/main/resources/static/media"
+
+/**
+ * The type of file being uploaded.
+ */
+enum class UploadType(dirName: String) {
+
+    ProfileImage("profiles"),
+    HeaderImage("headers"),
+    PostAttachment("posts");
+
+    /** The directory containing this type of media upload. */
+    val directory = "$LOCAL_DIR/$dirName/".apply {
+        val path = Paths.get(this)
+        if (Files.exists(path)) return@apply
+
+        Files.createDirectories(path)
+        if (Files.notExists(path))
+            throw IllegalStateException("Failed to create necessary media directory: $this")
+    }
+
+    /** The URL prefix of a file in this directory. */
+    val url = "/media/$dirName"
+
+}
+
+//
+// End points
+//
+
+/**
+ * Copies [this][MultipartFile] file from the hosts's computer to the
+ * directory corresponding with the given upload [type] with a new file name.
+ *
+ * @return The URL the file can now be accessed at.
+ */
+fun MultipartFile.writeAs(type: UploadType): String {
+    if (isEmpty) {
+        throw java.lang.IllegalStateException("Selected file is empty or does not exist!")
+    }
+
+    // generate the new file name based off of the current system time,
+    // which, admittedly, is probably a pretty bad idea, but whatever
+    val fileName = (originalFilename ?: name).let {
+        val extension = it.substringAfterLast('.')
+
+        findNewFileName(type.directory, extension)
+    }
+
+    // actually copy the file into its new location
+    val bytes = bytes
+    val path = Paths.get("${type.directory}/$fileName")
+    Files.write(path, bytes)
+
+    // now tell the user how to access that file
+    return "${type.url}/$fileName"
+}
+
+/**
+ * Finds a new file name based in part of the system's current time and in part
+ * on the JVM's monotnonic clock. The file will be guaranteed to not already
+ * exist in the given [path] with the given [extension].
+ */
+private fun findNewFileName(path: String, extension: String): String {
+    var name: String
+
+    // continue trying new names until we find one that doesn't
+    // already exist
+    do {
+
+        // generate a random named based off of:
+        // - the system's clock time in millis
+        // - the JVM's monotonic clock time
+        // together, these should probably never actually cause a
+        // name collision, but better safe than sorry
+        // also base 36 because it's easy and looks cool
+        val millisPart = System.currentTimeMillis().toString(radix = 36)
+        val nanosPart = System.nanoTime().toString(radix = 36)
+
+        name = "$millisPart$nanosPart.$extension"
+    } while (Files.exists(Paths.get("$path/$name")))
+
+    return name
+}
