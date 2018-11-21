9241866107bfbacfa77a5fbe787b4a8bae6e05e9
addonovan <austin@addonovan.com>
2018-11-21 14:16:00 -0600

    add js to hide irrelevant form

--- src/main/resources/static/script/post.js
+++ src/main/resources/static/script/post.js
@@ -1,1 +1,31 @@
-console.log("hello, world!");
+function showEventPost() {
+    $("#PostForm").addClass("hidden");
+    $("#EventForm").removeClass("hidden");
+}
+
+function showNormalPost() {
+    $("#PostForm").removeClass("hidden");
+    $("#EventForm").addClass("hidden");
+}
+
+function attachEventTypeListener() {
+    $("#PostTypeSelector input[name='postType']").change(function(event) {
+        switch ($(this).val()) {
+            case "post":
+                showNormalPost();
+                break;
+
+            case "event":
+                showEventPost();
+                break;
+
+            default:
+                console.error("Unrecognized post type option!");
+                break;
+        }
+    })
+}
+
+$(() => {
+    attachEventTypeListener();
+});
