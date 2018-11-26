e6623c769fe748c1a62b11014b67ccea1ad1028c
addonovan <austin@addonovan.com>
2018-11-26 14:00:35 -0600

    move testing data into its own up file

--- src/main/resources/db/up.sql
+++ src/main/resources/db/up.sql
@@ -152,23 +152,3 @@ CREATE TABLE "Follow" (
 --
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA "public" TO "application";
-
---
--- Insert Testing Data
---
-
-INSERT INTO "RefEmotion"
-    (emotionname, ImageURL)
-VALUES
-       ('Like', '/media/reactions/like.png'),
-       ('Anger', '/media/reactions/anger.png'),
-       ('Dislike', '/media/reactions/dislike.png'),
-       ('Love', '/media/reactions/love.png');
-
-INSERT INTO "RefLanguage"
-    (LocaleCode, LanguageName)
-VALUES
-       ('en_us', 'English (United States)'),
-       ('de_de', 'Deutsch (Deutschland)'),
-       ('en_tx', 'English (Texas)');
-
