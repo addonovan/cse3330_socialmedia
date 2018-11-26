9ee814f8886f9e8bf11786982232088928fd9ad3
addonovan <austin@addonovan.com>
2018-11-26 14:01:25 -0600

    move ref data back into up.sql

--- src/main/resources/db/up.sql
+++ src/main/resources/db/up.sql
@@ -152,3 +152,23 @@ CREATE TABLE "Follow" (
 --
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA "public" TO "application";
+
+--
+-- Insert Reference Data
+--
+
+INSERT INTO "RefEmotion"
+    (emotionname, ImageURL)
+VALUES
+       ('Like', '/media/reactions/like.png'),
+       ('Anger', '/media/reactions/anger.png'),
+       ('Dislike', '/media/reactions/dislike.png'),
+       ('Love', '/media/reactions/love.png');
+
+INSERT INTO "RefLanguage"
+    (LocaleCode, LanguageName)
+VALUES
+       ('en_us', 'English (United States)'),
+       ('de_de', 'Deutsch (Deutschland)'),
+       ('en_tx', 'English (Texas)');
+
