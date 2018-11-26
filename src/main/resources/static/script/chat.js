3592df63a2040f1ab476d5c3f973578a2852c43c
addonovan <austin@addonovan.com>
2018-11-25 23:07:46 -0600

    fix showing messages in group chats

--- src/main/resources/static/script/chat.js
+++ src/main/resources/static/script/chat.js
@@ -2,17 +2,17 @@ const fetchGroupInfo = (groupId, callback) =>
     $.getJSON("/chat/api/group/" + groupId, callback);
 
 const fetchGroupMembers = (groupId, callback) =>
-    $.getJSON("/chat/api/group/" + groupId, callback);
+    $.getJSON("/chat/api/members/" + groupId, callback);
 
 const fetchMessages = (groupId, callback) =>
-    $.getJSON("/chat/api/group/" + groupId, callback);
+    $.getJSON("/chat/api/messages/" + groupId, callback);
 
 function showMessages(groupInfo, groupMembers, messages) {
     function buildGroupOutline(groupInfo) {
         let outline = $("#Components #Outline").clone();
 
         // fill out the header
-        outline.find("#GroupHeader img.profileImage").attr('src', groupInfo.imageUrl);
+        outline.find("#GroupHeader img.profileImage").attr("src", groupInfo.imageUrl);
         outline.find("#GroupHeader #GroupName").text(groupInfo.name);
         outline.find("#GroupHeader #GroupDescription").text(groupInfo.name);
 
@@ -25,8 +25,8 @@ function showMessages(groupInfo, groupMembers, messages) {
     function buildMessageEntry(groupMembers, message) {
         let member = groupMembers[message.senderId];
         let item = $("#Components .historyItem").clone();
-        item.find("img.profileImage").src(member.profileImageUrl);
-        item.find("p.senderName").text(member.name);
+        item.find("img.profileImage").prop("src", member.profileImageURL);
+        item.find("p.senderName").text(member.firstName + " " + member.lastName);
         item.find("p.messageText").text(message.message);
         return item;
     }
